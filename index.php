87f9241b0c3abb383c15f91f8ebfe636c253a06f
Alexander Herrera <alherreras10@gmail.com>
2020-11-27 15:18:57 -0500

    template footer

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
     <script src="https://kit.fontawesome.com/e9fad0131d.js" crossorigin="anonymous"></script>
-
+    <link rel="stylesheet" href="./css/footer.css">
     <title>Inicio</title>
 </head>
 <body>

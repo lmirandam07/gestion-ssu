d2881315fc2a7af1fed4007835918cb20fcb7e5b
Lionel Henriquez <leo-henriquez@hotmail.es>
2020-11-27 13:32:52 -0500

    k :wq

---
+++ iniciarsesion.php
@@ -0,0 +1,35 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+    <title>Inicio</title>
+</head>
+<body>
+        <div class="container">
+            <h3>Iniciar Sesion</h3>
+            <br>
+            <form action="localhost:8000/" method="POST">
+            <div class="field">
+            <label class="label">Correo Electronico</label>
+        <div class="control">
+            <input class="input" type="text" placeholder="Text input">
+         </div>
+         <br>
+
+         <div class="field">
+            <label class="label">Contraseña</label>
+        <div class="control">
+            <input class="input is-success" type="text" placeholder="Text input">
+        </div>
+        </div>
+            </div>
+
+
+
+            </form>
+        </div>
+
+    </div>
+</body>
+</html>

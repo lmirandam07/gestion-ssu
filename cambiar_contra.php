6a295eb6ba75b3ac7f54bbe6fd06d0d1ea7b5cf8
Lionel Henriquez <leo-henriquez@hotmail.es>
2020-11-27 20:19:48 -0500

    se cambiaron nombres de archivo

--- cambiar_contra.php
+++ cambiar_contra.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
     <link rel="stylesheet" href="./css/header2.css">
-    <link rel="stylesheet" href="css/cambiarcont.css">
+    <link rel="stylesheet" href="css/cambiar_cont.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
         integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <title>Cambiar Contraseña</title>

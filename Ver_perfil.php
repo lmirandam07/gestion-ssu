aa10ea8c3f0208915dd1e888984b3991cdaad9f7
Lionel Henriquez <leo-henriquez@hotmail.es>
2020-11-29 16:39:20 -0500

    se crea pagina de ver perfil

---
+++ Ver_perfil.php
@@ -0,0 +1,75 @@
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
+    <link rel="stylesheet" href="./css/header.css">
+    <link rel="stylesheet" href="./css/footer.css">
+    <link rel="stylesheet" href="./css/ver_perfil.css">
+    <script src="https://kit.fontawesome.com/e9fad0131d.js" crossorigin="anonymous"></script>
+    <title>Ver Perfil</title>
+</head>
+<body>
+<?php include('./templates/header.html'); ?>
+
+<div class="hero is-fullheight is-primary">
+        <div class="hero-body ">
+            <div class="columns">
+                <div class="column columna1 ">
+           
+                    <div class="columns is-full">
+                        <div class="column is-full">
+                        <i class="far fa-user-circle fa-10x"></i>
+                        <i class="far fa-edit"></i>
+                            <h1>NOMBRE APELLIDO</h1>
+                            <h3 class="has-text-centered">ESTUDIANTE</h3>
+                            <h3>FECHA DE NACIMIENTO</h3>
+                            <H3>GÉNERO</H3>
+                            <H3>FACULTAD</H3>
+                            <H3>CORREO ELECTRONICO</H3>
+                            <H3>TELÉFONO</H3>
+                            <H3>CONTRASEÑA</H3>
+                            
+                        </div>
+                        <div class="column">
+                            <h3>0 de Enero de 0000</h3>
+                            <h3>Indefinido</h3>
+                            <h3>Sistemas</h3>
+                            <h3>nombredecorreo@correo.com</h3>
+                            <h3>0000-0000</h3>
+                            <h3>***********</h3>
+                            
+
+                        </div>
+                    </div>
+
+
+
+
+
+
+               
+            
+
+
+
+
+
+
+            </div>
+            <div class="column columna2"></div>
+                </div>
+            </div>
+
+    </div>
+
+
+
+
+
+
+<?php include('./templates/footer.html'); ?>
+
+    
+</body>
+</html>

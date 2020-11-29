f8dce5e1a148d5011aca11ff0ad4bdf06ffe3152
Lionel Henriquez <leo-henriquez@hotmail.es>
2020-11-29 14:52:16 -0500

    Se cambiaron los colores y arreglaron links

--- registrar.php
+++ registrar.php
@@ -4,8 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
-    <link rel="stylesheet" href="./css/header2.css">
-    <link rel="stylesheet" href="css/registrar.css">
+    <link rel="stylesheet" href="./css/header2_2.css">
+    <link rel="stylesheet" href="css/registrar2.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
         integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <title>Inicio</title>

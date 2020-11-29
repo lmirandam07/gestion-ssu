d619ea9c4539b0632ed8f563d634b74d4a7d2c3c
kirsten chong <kirstenchongutp@gmail.com>
2020-11-29 14:17:44 -0500

    Actualizacion y cambio de colores en contacto

--- contacto.php
+++ contacto.php
@@ -28,23 +28,23 @@
                             <p class="has-text-centered is-size-3 has-text-weight-medium">Correos Electrónicos</p>
                         </div>
                             <div class="column is-half is-gapless">
-                                <p class="has-text-left level-left ml-5"><span class="icon"><i class="far fa-envelope"></i></span><span>servicio.social@utp.ac.pa</span></p>
+                                <p class="has-text-centered level-item"><span class="icon"><i class="far fa-envelope"></i></span><span>servicio.social@utp.ac.pa</span></p>
                             </div>
                             <div class="column is-half is-gapless">
-                                <p class="has-text-left level-left ml-5"><span class="icon"><i class="far fa-envelope"></i></span><span>goldie.bravo@utp.ac.pa</span></p>
+                                <p class="has-text-centered level-item"><span class="icon"><i class="far fa-envelope"></i></span><span>goldie.bravo@utp.ac.pa</span></p>
                             </div>
                             <div class="column is-half is-gapless">
-                                <p class="has-text-left level-left ml-5"><span class="icon"><i class="far fa-envelope"></i></span><span>ayansin.delacruz@utp.ac.pa</span></p>
+                                <p class="has-text-centered level-item"><span class="icon"><i class="far fa-envelope"></i></span><span>ayansin.delacruz@utp.ac.pa</span></p>
                             </div>
                             <div class="column is-half is-gapless">
-                                <p class="has-text-left level-left ml-5"><span class="icon"><i class="far fa-envelope"></i></span><span>marjorie.franceschi@utp.ac.pa</span></p>
+                                <p class="has-text-centered level-item"><span class="icon"><i class="far fa-envelope"></i></span><span>marjorie.franceschi@utp.ac.pa</span></p>
                             </div>
                         <div class="column is-offset-12 is-gapless"></div>
                         <div class="column is-offset-12 is-gapless"></div>
                         <div class="column is-offset-12 is-gapless"></div>
                     </div>
             </div>
-        <div  class="column is-half is-gapless container_darkgray">
+        <div  class="column is-half is-gapless container_darkgray p-5">
                 <div class="columns is-centered is-multiline">
                     <div class="column is-offset-12"></div>
                     <div class="column is-offset-12"></div>
@@ -52,16 +52,16 @@
                         <p class="has-text-centered is-size-3 has-text-weight-medium">Teléfonos</p>
                     </div class="has-text-centered">
                     <div class="column is-half is-gapless">
-                        <p class="has-text-left level-left ml-6"><span class="icon"><i class="fas fa-phone-alt mr-2"></i></span><span>(507) 560-3689</span></p>
+                        <p class="has-text-centered level-item"><span class="icon"><i class="fas fa-phone-alt mr-2"></i></span><span>(507) 560-3689</span></p>
                     </div>
                     <div class="column is-half is-gapless">
-                        <p class="has-text-left level-left ml-6"><span class="icon"><i class="fas fa-phone-alt mr-2"></i></span><span>(507) 560-3705</span></p>
+                        <p class="has-text-centered level-item"><span class="icon"><i class="fas fa-phone-alt mr-2"></i></span><span>(507) 560-3705</span></p>
                     </div>
                     <div class="column is-half is-gapless">
-                        <p class="has-text-left level-left ml-6"><span class="icon"><i class="fas fa-phone-alt mr-2"></i></span><span>(507) 560-3689</span></p>
+                        <p class="has-text-centered level-item"><span class="icon"><i class="fas fa-phone-alt mr-2"></i></span><span>(507) 560-3689</span></p>
                     </div>
                     <div class="column is-half is-gapless">
-                        <p class="has-text-left level-left ml-6"><span class="icon"><i class="fas fa-phone-alt mr-2"></i></span><span>(507) 560-3689</span></p>
+                        <p class="has-text-centered level-item"><span class="icon"><i class="fas fa-phone-alt mr-2"></i></span><span>(507) 560-3689</span></p>
                     </div>
                     <div class="column is-offset-12 is-gapless"></div>
                     <div class="column is-offset-12 is-gapless"></div>

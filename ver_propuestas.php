412a8f87f15a810f3e61ab16fe34701baca1e659
Javier Alejandro Singh <javieralejandrosingh15@hotmail.com>
2020-11-29 20:27:41 -0500

    Links fontawesome 2.0

--- ver_propuestas.php
+++ ver_propuestas.php
@@ -6,6 +6,8 @@
     <link rel="stylesheet" href="./css/header.css">
     <link rel="stylesheet" href="./css/footer.css">
     <link rel="stylesheet" href="./css/ver_proyectos_propuestas.css">
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
+        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <title>Ver Propuestas de Proyecto</title>
 </head>
 <body>

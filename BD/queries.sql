76c56a86124379ad8e9656ad847099439747d59a
kirsten chong <kirstenchongutp@gmail.com>
2020-11-30 15:30:14 -0500

    Queries tablas BD

---
+++ BD/queries.sql
@@ -0,0 +1,44 @@
+USE ssu_db;
+
+CREATE TABLE estado_proyecto (id_estado_pro INT NOT NULL, estado_pro VARCHAR(50) NOT NULL, 
+                            CONSTRAINT pk_id_estado_pro PRIMARY KEY (id_estado_pro)
+                            );
+
+CREATE TABLE facultad (id_facultad INT NOT NULL, nombre_facultad VARCHAR(80) NOT NULL,
+                        CONSTRAINT pk_id_facultad PRIMARY KEY (id_facultad)
+                        );
+
+CREATE TABLE facultad_propuesta (id_prop INT NOT NULL, id_facultad INT NOT NULL,
+                        CONSTRAINT pk_id_prop_facultad PRIMARY KEY (id_prop, id_facultad)
+                        );                    
+
+CREATE TABLE propuesta_proyecto (id_prop INT NOT NULL, nombre_pro VARCHAR(50) NOT NULL, lugar_pro VARCHAR(50) NOT NULL, fecha_pro DATE NOT NULL, 
+                                hora_inicio_pro TIME NOT NULL, hora_final_pro TIME NOT NULL, participantes_pro INT NOT NULL, descripcion_pro VARCHAR(300) NOT NULL,
+                                objetivo_pro VARCHAR(300) NOT NULL, materiales_pro VARCHAR(300) NOT NULL, nombre_encarg VARCHAR(50) NOT NULL, cedula_encarg VARCHAR(20) NOT NULL,
+                                telefono_encarg INT NOT NULL, correo_encarg VARCHAR(50) NOT NULL, id_estado INT NOT NULL, perfil_estu_pro VARCHAR(300) NOT NULL,
+                                CONSTRAINT pk_id_prop PRIMARY KEY (id_prop),
+                                CONSTRAINT fk_id_estado FOREIGN KEY (id_estado) REFERENCES estado_proyecto (id_estado_pro)
+                                );
+
+CREATE TABLE proyecto (id_pro INT NOT NULL ,nombre_pro VARCHAR(50) NOT NULL, lugar_pro VARCHAR(50) NOT NULL, fecha_pro DATE NOT NULL, 
+                                hora_inicio_pro TIME NOT NULL, hora_final_pro TIME NOT NULL, participantes_pro INT NOT NULL, descripcion_pro VARCHAR(300) NOT NULL,
+                                objetivo_pro VARCHAR(300) NOT NULL, materiales_pro VARCHAR(300) NOT NULL, nombre_encarg VARCHAR(50) NOT NULL, cedula_encarg VARCHAR(20) NOT NULL,
+                                telefono_encarg INT NOT NULL, correo_encarg VARCHAR(50) NOT NULL, perfil_estu_pro VARCHAR(300) NOT NULL,
+                                CONSTRAINT pk_id_pro PRIMARY KEY (id_pro)
+                                );
+
+CREATE TABLE ano_proyecto (id_prop INT NOT NULL, id_ano INT NOT NULL,
+                            CONSTRAINT  pk_prop_ano PRIMARY KEY (id_prop, id_ano) 
+                            );
+
+CREATE TABLE ano (id_ano INT NOT NULL, ano_estudio VARCHAR(20) NOT NULL,
+                    CONSTRAINT pk_id_ano PRIMARY KEY (id_ano)
+                );
+
+CREATE TABLE proyecto_usuario (id_pro INT NOT NULL, id_usuario INT NOT NULL, horas_usuario INT DEFAULT 0,
+                                CONSTRAINT pk_por_usuario PRIMARY KEY (id_pro, id_usuario)
+                                );
+
+CREATE TABLE tipo_usuario (id_tipo_us INT NOT NULL, tipo_usuario VARCHAR(50) NOT NULL,
+                            CONSTRAINT pk_id_tipo_us PRIMARY KEY (id_tipo_us)
+                            );

f0c1595e14db19fbd96d733a2260e91176207856
Lionel Henriquez <leo-henriquez@hotmail.es>
2020-12-02 10:29:38 -0500

    prueba

--- Entidades/Usuario.php
+++ Entidades/Usuario.php
@@ -5,17 +5,28 @@
 
 class Usuario{
     private $db;
+    private $registro;
+
     
 
     public function contruct(){
         $this->db = db::conexion();
+        $this->registro = array();
     }
 
     public function registrarUsuarios($parametro){
-        $nombre=$parametro['']
+        $nombre=$parametro['nombre'];
+        $apellido=$parametro['apellido'];
+        $cedula=$parametro['cedula'];
+        $numero_contacto=$parametro['numero_contaco'];
+        $correo=$parametro['correo'];
+        $contra=$parametro['contra'];
+        $facultad=$parametro['facultad'];
 
-        $consulta = $this->db->query("INSERT INTO usuario (nombre_us, apellido_us, cedula_us, id_tipo_us, telefono, correo, contrasena, facultad) 
-        VALUES ('$nombre', '$apellido', '$cedula', '1', '$numero_contacto', '$correo', '$contra', '$facultad');");
+        $sql="INSERT INTO usuario (nombre_us, apellido_us, cedula_us, id_tipo_us, telefono, correo, contrasena, facultad) 
+        VALUES ('$nombre', '$apellido', '$cedula', '1', '$numero_contacto', '$correo', '$contra', '$facultad')";
+
+        $this->db->query($sql);
     }
 
 

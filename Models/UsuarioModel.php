615731668458e2b41fa195da1d0b89e346344f23
Lionel Henriquez <leo-henriquez@hotmail.es>
2020-12-02 19:26:51 -0500

    cambio en usuariomodel

--- Models/UsuarioModel.php
+++ Models/UsuarioModel.php
@@ -25,15 +25,6 @@ class UsuarioModel{
         $contra=$datos['contrasena'];
         $facultad=$datos['facultad'];
 
-        echo $nombre;
-        echo $apellido;
-        echo $cedula;
-        echo $numero_contacto;
-        echo $contra;
-        echo $correo;
-        echo $facultad;
-
-
         $sql="INSERT INTO usuario (nombre_us, apellido_us, cedula_us, id_tipo_us, telefono, correo, contrasena, total_horas,facultad) 
         VALUES ('$nombre', '$apellido', '$cedula', 1, '$numero_contacto', '$correo', '$contra', 0, '$facultad')";
         if($this->db->query($sql) == True){

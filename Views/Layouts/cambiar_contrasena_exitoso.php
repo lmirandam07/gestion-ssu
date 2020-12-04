ff10872f4a664f1394cd311ca5877bce25f0b5e3
kirsten chong <kirstenchongutp@gmail.com>
2020-12-03 23:40:40 -0500

    Modals de correo electronico

--- Views/Layouts/cambiar_contrasena_exitoso.php
+++ Views/Layouts/cambiar_contrasena_exitoso.php
@@ -1,18 +1,17 @@
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
-    <link rel="stylesheet" href="./css/header2.css">
-    <link rel="stylesheet" href="./css/cambiar_cont.css">
+    <link rel="stylesheet" href="../../css/header2.css">
+    <link rel="stylesheet" href="../../css/cambiar_cont.css">
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
         integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <title>Cambiar Contraseña</title>
 </head>
 
 <body>
-<?php include('./Views/Layouts/header2.html'); ?>
+<?php include('header2.html'); ?>
 
     <div class="hero is-fullheight">
         <div class="hero-body ">
@@ -55,9 +54,26 @@
     </div>
 
 
-
-
-
+<div class="modal is-active">
+    <div class="modal-background"></div>
+    <div class="modal-card has-text-centered">
+      <header class="modal-card-head">
+        <p class="modal-card-title">Cambiar Contraseña</p>
+        
+      </header>
+      <section class="modal-card-body">
+       <p class="medium">Para completar el cambio de contraseña, abrir el enlace enviado a su correo electronico</p>
+      </section>
+      <footer class="modal-card-foot columns">
+        <div class="column"></div>
+        <div class="column is-two-fifths">
+            <a href="../index.php"><button class="button is-principal">Ok</button></a>
+        </div>
+        <div class="column"></div>
+          
+      </footer>
+    </div>
+  </div>
 
 </body>
 

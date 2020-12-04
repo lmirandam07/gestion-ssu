84769ce416d65a64bb973228e01f6413b804d5d4
Luis <luyimiranda07@gmail.com>
2020-12-04 10:38:23 -0500

    Corrección de rutas en footer y header

--- cambiar_contra.php
+++ cambiar_contra.php
@@ -6,13 +6,12 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.1/css/bulma.min.css">
     <link rel="stylesheet" href="./css/header2.css">
     <link rel="stylesheet" href="./css/cambiar_cont.css">
-    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css"
-        integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
     <title>Cambiar Contraseña</title>
 </head>
 
 <body>
-<?php include('./Views/Layouts/header2.html'); ?>
+    <?php include('./Views/Layouts/header2.html'); ?>
 
     <div class="hero is-fullheight">
         <div class="hero-body ">
@@ -20,32 +19,32 @@
                 <div class="column is-10 is-offset-1">
 
 
-<div class="box">
-<h3 class="title titulo">Cambiar Contraseña</h3>
+                    <div class="box">
+                        <h3 class="title titulo">Cambiar Contraseña</h3>
 
-  <form action="./route.php?controller=Cambiar_Contrasena" method="POST">
-  <div class="field">
-    <div class="control">
-    <label class="label has-text-left label1">Correo Registrado</label>
-      <input class="input is-medium input-is" type="email" name="correo" required>
-    </div>
-  </div>
-  <div class="field">
-    <div class="control">
-    <label class="label has-text-left">Nueva Contraseña</label>
-      <input class="input is-medium input-is" type="text" name="nueva_contra" autofocus="" required>
-    </div>
-  </div>
-  <div class="field">
-    <div class="control">
-    <label class="label has-text-left ">Reescribir Contraseña</label>
-      <input class="input is-medium input-is" type="text" name="verif_nueva_contra" required>
-    </div>
-  </div>
-  <input class="button boton" type="submit" value="Cambiar Contraseña">
-</form>
+                        <form action="./route.php?controller=Cambiar_Contrasena" method="POST">
+                            <div class="field">
+                                <div class="control">
+                                    <label class="label has-text-left label1">Correo Registrado</label>
+                                    <input class="input is-medium input-is" type="email" name="correo" required>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <label class="label has-text-left">Nueva Contraseña</label>
+                                    <input class="input is-medium input-is" type="text" name="nueva_contra" autofocus="" required>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="control">
+                                    <label class="label has-text-left ">Reescribir Contraseña</label>
+                                    <input class="input is-medium input-is" type="text" name="verif_nueva_contra" required>
+                                </div>
+                            </div>
+                            <input class="button boton" type="submit" value="Cambiar Contraseña">
+                        </form>
 
-</div>
+                    </div>
 
                 </div>
             </div>

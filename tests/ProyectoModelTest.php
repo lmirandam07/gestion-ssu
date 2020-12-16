ccb52613bbbee4e252845e9ff2cce2597a2ea495
Alexander Herrera <alherreras10@gmail.com>
2020-12-15 22:48:17 -0500

    Pruebas para Proyecto Model

--- tests/ProyectoModelTest.php
+++ tests/ProyectoModelTest.php
@@ -5,19 +5,50 @@ use \PHPUnit\Framework\TestCase;
 
 class ProyectoModelTest extends TestCase {
 
-    public function Proveedor(){
+    /*public function proveedorObtenerProyectos(){
         return[
-            'Caso 1' => []
-        ]
+            'Caso ' => [1,"Limpieaza de playa","Ayudemos al mar.",[4]],
+            'Caso 1' => ["id_proyecto"=>1,"nombre_pro"=>"Limpieza de playa","descripcion_pro"=>]
+        ];
     }
 
     /**
-    * @dataProvider Proveedor1
+    * @dataProvider proveedorObtenerProyectos
     */
-    public function funcion1()
-    {
-        $Propuesta = new PropuestaModel();
+   /* public function testObtenerProyectos($resultado_esperado,$page)
+    { 
+        $proyecto = new ProyectoModel();
+        $this->assertEquals($resultado_esperado, $proyecto->obtener_proyecto($page));
        
+    }*/
+
+    public function testObtenerDatosProyectos(){
+        $stub = $this->createMock(ProyectoModel::class);
+
+        $stub->method('obtener_proyecto')
+            ->willReturn(array($proyectos));
+            
+
+
+    }
+    
+
+
+    public function proveedorEstudianteInscrito(){
+
+        return[
+            'Caso1' => ["correo"=>"carlos@gmail.com","id_propuesta"=>2,True]
+        ];
+        
+        
+    }
+    /**
+    * @dataProvider proveedorEstudianteInscrito
+    */
+    public function testEstudianteInscrito($datos1,$datos2,$resultado_esperado){
+        $proyecto = new ProyectoModel();
+        $this->assertEquals($resultado_esperado,$proyecto->estudiante_inscrito($datos1,$datos2));
+
     }
 
 }
